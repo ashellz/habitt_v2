64df8716659944c28d3ff7b13f3beea1f63f3981
ashellz <101022734+ashellz@users.noreply.github.com>
2025-03-22 13:55:52 +0100

    custom text field, habit name

--- lib/pages/other_pages/add_habit_page.dart
+++ lib/pages/other_pages/add_habit_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:habitt/pages/main_pages/habits_page.dart';
 import 'package:habitt/providers/color_provider.dart';
 import 'package:habitt/widgets/habits_page/categories/categories_list.dart';
@@ -14,7 +15,7 @@ class AddHabitPage extends StatelessWidget {
     final localizations = AppLocalizations.of(context)!;
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(backgroundColor: Colors.transparent),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: ListView(
@@ -34,7 +35,91 @@ class AddHabitPage extends StatelessWidget {
               standardColor: true,
               habitsCount: false,
             ),
+            CustomTextField(
+              title: localizations.habitName,
+              controller: TextEditingController(),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.title,
+    required this.controller,
+    this.maxTextLength = 9999,
+  });
+
+  final String title;
+  final TextEditingController controller;
+  final int maxTextLength;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorProvider colorProvider = context.watch<ColorProvider>();
+
+    return Padding(
+      padding: EdgeInsets.only(top: 24),
+      child: TextFormField(
+        textInputAction: TextInputAction.done,
+        keyboardAppearance:
+            Theme.of(context).brightness == Brightness.dark
+                ? Brightness.dark
+                : Brightness.light,
+        inputFormatters: [LengthLimitingTextInputFormatter(maxTextLength)],
+        cursorColor: colorProvider.textColor,
+        cursorWidth: 1.0,
+        cursorHeight: 20.0,
+        cursorRadius: const Radius.circular(12.0),
+        cursorOpacityAnimates: true,
+        enableInteractiveSelection: true,
+
+        // Main input text style
+        style: TextStyle(color: colorProvider.textColor, fontSize: 14),
+
+        // Decoration
+        decoration: InputDecoration(
+          // Borders
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+            borderSide: BorderSide(
+              color: colorProvider.colorScheme.strokeColor,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: colorProvider.colorScheme.strokeColor,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+            borderSide: BorderSide(
+              color: colorProvider.colorScheme.strokeColor,
+            ),
+          ),
+
+          // Fill color
+          filled: true,
+          fillColor: colorProvider.standardColor,
+
+          // Content padding
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: 16,
+          ),
+
+          //Label (Title)
+          labelStyle: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: colorProvider.textColor,
+          ),
+          labelText: title,
         ),
       ),
     );
