8c4c5598385d251ae0da469505c1e923bb15a955
ashellz <101022734+ashellz@users.noreply.github.com>
2025-06-29 08:33:36 +0200

    glasscontainer

--- lib/widgets/habit_widget/habit_completion/habit_completion.dart
+++ lib/widgets/habit_widget/habit_completion/habit_completion.dart
@@ -156,6 +156,8 @@ class _CompletionDisplayState extends State<CompletionDisplay> {
                 ),
               ),
 
+              GlassContainer(),
+
               Padding(
                 padding: const EdgeInsets.all(4.0),
                 child: AnimatedSwitcher(
@@ -245,4 +247,57 @@ void showAnimatedBlurDialog(BuildContext context, Habit habit) {
       );
     },
   );
+}
+
+class GlassContainer extends StatelessWidget {
+  final double height;
+  final double width;
+  final Widget? child;
+  final double borderRadius;
+  final double blur;
+  final Color borderColor;
+  final Gradient gradient;
+  //You can configure other properties as well
+
+  const GlassContainer({
+    super.key,
+    this.height = 200,
+    this.width = 300,
+    this.child,
+    this.borderRadius = 15,
+    this.blur = 10,
+    this.borderColor = Colors.white24,
+    this.gradient = const LinearGradient(
+      colors: [Colors.white24, Colors.white10],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+    ),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: Stack(
+        children: [
+          // Blur effect
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+            child: Container(),
+          ),
+          // Gradient overlay
+          Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+              gradient: gradient,
+              border: Border.all(color: borderColor),
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
 }
