69456aac768640344f3c628376fc8b871178b2f8
ashellz <101022734+ashellz@users.noreply.github.com>
2025-12-13 07:44:01 +0100

    fix for scheduler

--- lib/widgets/habit_details/select_habit_time_page/all_habits_on_time_line_stack.dart
+++ lib/widgets/habit_details/select_habit_time_page/all_habits_on_time_line_stack.dart
@@ -17,123 +17,127 @@ class AllHabitsOnTimelineStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tp = context.watch<ThemeProvider>();
-    final habits = context.watch<HabitProvider>().habits;
+    final habitsRaw = context.watch<HabitProvider>().habits;
+    // remove the habits that are not enabled for time intervals and are not the one to ignore
+    final habits =
+        habitsRaw
+            .where(
+              (habit) =>
+                  habit.timeIntervalEnabled &&
+                  (ignoreId == null || habit.id != ignoreId),
+            )
+            .toList();
 
     return Stack(
       children: [
         for (var habit in habits)
-          if (habit.timeIntervalEnabled && habit.id != ignoreId)
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.fastOutSlowIn,
+            top: habit.getStartHour() * hourHeight + hourHeight / 2,
+            left: 60,
+            right: 20,
+            height:
+                habit.getTimeType() != TimeType.regular
+                    ? (24 * hourHeight) - (habit.getStartHour() * hourHeight)
+                    : habit.getTimeDuration() * hourHeight,
+            child: Container(
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: habit.getContainerColor(tp),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 4,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: habit.getNameColor(tp),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    switchInCurve: Curves.easeOut,
+                    switchOutCurve: Curves.easeIn,
+                    transitionBuilder: (child, animation) {
+                      final offsetAnimation = Tween<Offset>(
+                        begin: const Offset(0.0, 0.2),
+                        end: Offset.zero,
+                      ).animate(animation);
+
+                      return SlideTransition(
+                        position: offsetAnimation,
+                        child: FadeTransition(opacity: animation, child: child),
+                      );
+                    },
+                    child: KeyedSubtree(
+                      key: ValueKey<bool>(
+                        habit.shouldShowName(habit.getTimeType()),
+                      ),
+                      child:
+                          habit.shouldShowName(habit.getTimeType())
+                              ? Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                spacing: 4,
+                                children: [
+                                  Image.asset(
+                                    habit.iconPath,
+                                    width: 24,
+                                    height: 24,
+                                  ),
+                                  Text(
+                                    habit.name,
+                                    style: TextStyle(
+                                      color: habit.getNameColor(tp),
+                                      fontWeight: FontWeight.w500,
+                                      letterSpacing: 1,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              )
+                              : Container(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+        for (var habit in habits)
+          if (habit.getTimeType() == TimeType.overday)
             AnimatedPositioned(
               duration: const Duration(milliseconds: 500),
               curve: Curves.fastOutSlowIn,
-              top: habit.getStartHour() * hourHeight + hourHeight / 2,
+              top: hourHeight / 2,
               left: 60,
               right: 20,
-              height:
-                  habit.getTimeType() != TimeType.regular
-                      ? (24 * hourHeight) - (habit.getStartHour() * hourHeight)
-                      : habit.getTimeDuration() * hourHeight,
+              height: habit.timeIntervalEnd / 60 * hourHeight,
               child: Container(
                 padding: EdgeInsets.all(4),
                 decoration: BoxDecoration(
                   color: habit.getContainerColor(tp),
                   borderRadius: BorderRadius.circular(8),
                 ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 4,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        color: habit.getNameColor(tp),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: 4,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: habit.getNameColor(tp),
+                      borderRadius: BorderRadius.circular(4),
                     ),
-                    SizedBox(width: 5),
-                    AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 500),
-                      switchInCurve: Curves.easeOut,
-                      switchOutCurve: Curves.easeIn,
-                      transitionBuilder: (child, animation) {
-                        final offsetAnimation = Tween<Offset>(
-                          begin: const Offset(0.0, 0.2),
-                          end: Offset.zero,
-                        ).animate(animation);
-
-                        return SlideTransition(
-                          position: offsetAnimation,
-                          child: FadeTransition(
-                            opacity: animation,
-                            child: child,
-                          ),
-                        );
-                      },
-                      child: KeyedSubtree(
-                        key: ValueKey<bool>(
-                          habit.shouldShowName(habit.getTimeType()),
-                        ),
-                        child:
-                            habit.shouldShowName(habit.getTimeType())
-                                ? Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  spacing: 4,
-                                  children: [
-                                    Image.asset(
-                                      habit.iconPath,
-                                      width: 24,
-                                      height: 24,
-                                    ),
-                                    Text(
-                                      habit.name,
-                                      style: TextStyle(
-                                        color: habit.getNameColor(tp),
-                                        fontWeight: FontWeight.w500,
-                                        letterSpacing: 1,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ],
-                                )
-                                : Container(),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
-
-        for (var habit in habits)
-          if (habit.timeIntervalEnabled)
-            if (habit.getTimeType() == TimeType.overday)
-              AnimatedPositioned(
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.fastOutSlowIn,
-                top: hourHeight / 2,
-                left: 60,
-                right: 20,
-                height: habit.timeIntervalEnd / 60 * hourHeight,
-                child: Container(
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    color: habit.getContainerColor(tp),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      width: 4,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        color: habit.getNameColor(tp),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
       ],
     );
   }
