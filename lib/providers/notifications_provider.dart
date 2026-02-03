543c490af3c2568e10f99d413273cf9d59d4bc32
ashellz <101022734+ashellz@users.noreply.github.com>
2026-02-03 17:53:00 +0100

    notifications progress

--- lib/providers/notifications_provider.dart
+++ lib/providers/notifications_provider.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+enum NotificationPeriod {
+  morning,
+  afternoon,
+  evening;
+
+  String get name {
+    switch (this) {
+      case NotificationPeriod.morning:
+        return "Morning";
+      case NotificationPeriod.afternoon:
+        return "Afternoon";
+      case NotificationPeriod.evening:
+        return "Evening";
+    }
+  }
+}
+
 class NotificationsProvider extends ChangeNotifier {
   bool notificationsEnabled = false;
 }
