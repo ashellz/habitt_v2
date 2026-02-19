ea763f1de20f7cde7529253d4f52bda68aa9dd0b
ashellz <101022734+ashellz@users.noreply.github.com>
2026-02-19 12:42:26 +0100

    no habits widget

---
+++ lib/widgets/main_page/habits/habit_widget/new_habit_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:habitt/models/habit.dart';
+import 'package:habitt/services/new_color_service.dart';
+
+import 'package:provider/provider.dart';
+
+class NewHabitWidget extends StatelessWidget {
+  const NewHabitWidget({super.key, required this.habit});
+
+  final Habit habit;
+
+  @override
+  Widget build(BuildContext context) {
+    final cp = context.watch<ColorProvider>();
+
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: ShapeDecoration(
+        color: cp.widget,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: cp.border),
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 42,
+            height: 42,
+            clipBehavior: Clip.antiAlias,
+            decoration: ShapeDecoration(
+              color: cp.habitIconBg,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/main_page/habits/new_habit_category.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:habitt/models/category.dart';
+import 'package:habitt/models/habit.dart';
+import 'package:habitt/pages/main_pages/main_page.dart';
+import 'package:habitt/widgets/main_page/habits/habit_widget/new_habit_widget.dart';
+
+class NewHabitCategory extends StatefulWidget {
+  const NewHabitCategory({
+    super.key,
+    this.isFirst = false,
+    required this.showAdditionalTasks,
+    required this.category,
+    required this.habits,
+    required this.isToday,
+  });
+
+  final bool isFirst;
+  final Category category;
+  final bool showAdditionalTasks;
+  final List<Habit> habits;
+  final bool isToday;
+
+  @override
+  State<NewHabitCategory> createState() => _NewHabitCategoryState();
+}
+
+class _NewHabitCategoryState extends State<NewHabitCategory> {
+  double _opacity = 0; // For initial fade-in
+
+  @override
+  void initState() {
+    super.initState();
+    // Original fade-in animation
+    Future.delayed(Duration.zero, () {
+      if (mounted) {
+        setState(() {
+          _opacity = 1;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categoryHabits =
+        widget.habits
+            .where(
+              (habit) =>
+                  habit.categoryId == widget.category.id && !habit.additional,
+            )
+            .toList(); // It will not show additional habits/tasks
+
+    return AnimatedOpacity(
+      opacity: _opacity, // For the initial fade-in of the whole category block
+      duration: const Duration(milliseconds: 150),
+      child: Column(
+        spacing: 10,
+        children: [
+          // Using the new ScrollTransformedHabitCategoryTitle
+          if (categoryHabits.isNotEmpty)
+            NewHabitCategoryTitle(
+              category: widget.category,
+              countAdditionalTasks: widget.showAdditionalTasks,
+            ),
+          for (final habit in categoryHabits) NewHabitWidget(habit: habit),
+          if (widget.showAdditionalTasks) Container(),
+          // additional tasks
+        ],
+      ),
+    );
+  }
+}
