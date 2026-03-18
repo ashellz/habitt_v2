4b2ad9b88852250e94cf6a638782f6cb85cf6b88
ashellz <101022734+ashellz@users.noreply.github.com>
2026-03-18 08:43:05 +0100

    custom schedule dialog

--- lib/widgets/dialogs/schedules/custom_schedule_dialog.dart
+++ lib/widgets/dialogs/schedules/custom_schedule_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:habitt/services/new_color_service.dart';
 import 'package:habitt/widgets/default/new_default_dialog.dart';
 import 'package:habitt/widgets/dialogs/schedules/set_schedule_dialog.dart';
@@ -24,7 +25,48 @@ class CustomScheduleDialog extends StatelessWidget {
           builder: (context) => SetScheduleDialog(),
         );
       },
-      child: Column(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Repeat every:',
+            style: TextStyle(
+              color: cp.text,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: 12),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            clipBehavior: Clip.antiAlias,
+            decoration: ShapeDecoration(
+              color: cp.field,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Other day",
+                  style: TextStyle(fontSize: 16, color: cp.text),
+                ),
+                SvgPicture.asset("assets/images/new-svg/dropdown.svg"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: Text(
+              'This habit will appear every other day starting from today',
+              style: TextStyle(color: cp.greyText, fontSize: 13),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
