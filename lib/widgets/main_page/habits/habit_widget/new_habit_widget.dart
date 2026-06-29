8a8de1abf76f904f89c62355a825578e900d7c55
ashellz <101022734+ashellz@users.noreply.github.com>
2026-06-29 12:57:36 +0200

    vb vc

--- lib/widgets/main_page/habits/habit_widget/new_habit_widget.dart
+++ lib/widgets/main_page/habits/habit_widget/new_habit_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:habitt/models/habit.dart';
 import 'package:habitt/providers/category_provider.dart';
 import 'package:habitt/providers/color_provider.dart';
