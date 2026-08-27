87bfc032fe406fd76b13daac4c5322dfe363acfa
ashellz <101022734+ashellz@users.noreply.github.com>
2026-08-27 13:34:55 +0200

    android and apple req

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.shellz.habitt"
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    compileSdkVersion("android-37.0")
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -35,7 +35,7 @@ android {
         applicationId = "com.shellz.habitt"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
